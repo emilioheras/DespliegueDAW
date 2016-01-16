5dfaa83d1ed6daf88013ddac1cb8610e1494be22
emilioheras <emirobor@gmail.com>
2016-01-16 22:10:37 +0100

    Version inicial

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8">
+	<link rel="stylesheet" type="text/css" href="css/styles.css">
+</head> 
+<body>
+
+<h1>Icono del día</h1>
+
+<img src="randomicon.php"/>
+
+</body>
+</html>
+
+
+
