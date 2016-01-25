22d8ae4ee0135cbcfe990ed20595078963f45537
emilioheras <emirobor@gmail.com>
2016-01-25 19:02:30 +0100

    Creacion del script despliegue.php para practica de hooks

---
+++ despliegue.php
@@ -0,0 +1,15 @@
+<?php
+
+    if (isset($_POST['payload'])) {
+        
+        //Obtener los datos de GitHub.
+        $payload = json_decode($_POST['payload']);
+        
+        if ($payload->ref == "refs/heads/master") {
+		
+            //ejecutar el script del servidor.
+            //shell_exec('../PUESTA_EN_PRODUCCION.sh');
+        }
+    }
+    
+?>
